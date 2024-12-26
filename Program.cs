d7404932eae20603efb698caed90cfc658682477
student <student@E60760>
2024-12-26 11:44:41 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_1226_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                            Puppy DennisPuppy = new Puppy();
+                //設定DennisPuppy的屬性值.
+                DennisPuppy.Id = 112;
+                DennisPuppy.Owner = "Dennis";
+                DennisPuppy.Description = "cute";
+                DennisPuppy.Price = 12000;
+                DennisPuppy.Age = 1;
+                DennisPuppy.Type = "哈士奇";
+                DennisPuppy.money = 6500;
+                //類堆..所有屬性都要設.
+
+                //最後,叫用DennisPuppy的方法PrintAllAttrib( )印出所有屬性參數.
+                DennisPuppy.PrintAllAttrib();
+
+                //===============================================
+                //第二個 使用有參數的建構子設一隻寵物MarysPuppy.
+                //經由建構子函式2 來設定所有屬性
+                Puppy MarysPuppy = new Puppy(123, "Mary", "pretty", 15500, 1, "薩摩耶", 5500);
+
+                //最後,叫用 MarysPuppy的方法PrintAllAttrib( )印出所有屬性參數.
+                MarysPuppy.PrintAllAttrib();
+
+
+            }
+        }
+    }
+
+
---
+++ Puppy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_1226_001
+{
+    internal class Puppy
+    {
+                    //屬性
+            public int Id { get; set; }  //寵物哀低
+            public string Owner { get; set; }  //寵物主人
+            public string Description { get; set; } //寵物描述
+            public double Price { get; set; }//寵物價格
+            public double Age { get; set; } //寵物年紀
+            public string Type { get; set; }  //寵物品種
+
+            private double _money; //伙食費
+            public double money
+            {
+                get { return this._money; }
+                set { this._money = value; }
+            }
+
+            //建構子函式1
+            public Puppy() { }
+
+            //建構子函式2 
+            public Puppy(int id, string vOwner, string description, double price, double Age1, string type, double money1)
+            {
+                Id = id;
+                Owner = vOwner;
+                Description = description;
+                Price = price;
+                Age = Age1;
+                Type = type;
+                money = money1;
+
+            }
+
+            public void PrintAllAttrib()
+            {
+                Console.WriteLine("ID is:" + this.Id.ToString());
+                Console.WriteLine("Owener is :" + this.Owner.ToString());
+                Console.WriteLine("Description is :" + this.Description.ToString());
+                Console.WriteLine("Price is :" + this.Price.ToString());
+                Console.WriteLine("Age is :" + this.Age.ToString());
+                Console.WriteLine("Type is :" + this.Type.ToString());
+                Console.WriteLine("money is :" + this.money.ToString());
+                //類推..把所有屬性印出來.
+
+            }
+        }
+    }
